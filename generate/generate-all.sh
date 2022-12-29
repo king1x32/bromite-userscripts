2c6e2eca4c55141c5e8e88ca8ea63eef08c5c427
luxysiv <manhduonghna@gmail.com>
2022-12-29 14:06:49 +0700

    Update: 2022-12-29T07:07:34+00:00

---
+++ generate/generate-all.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash 
+set -euo pipefail 
+
+echo "::group::Generating IDCAC script"
+cd idcac
+bash ./generate.sh
+cd ..
+echo "::endgroup::"
+
+
+echo "::group::Generating cosmetic filter script"
+cd cosmetic
+bash ./generate.sh
+cd ..
+echo "::endgroup::"
+
+
+echo "::group::Generating release summary"
+cd release
+bash ./generate.sh
+cd ..
+echo "::endgroup::"
+
+

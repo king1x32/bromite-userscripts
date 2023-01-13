bb0ff56798271fa903ab0066e78b0b1ce909cd07
luxysiv <luxysiv@users.noreply.github.com>
2023-01-13 01:00:05 +0000

    Update userscripts: 2023-01-13

--- block/cosmetic.user.js
+++ block/cosmetic.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         Cosmetic Ad Block for Bromite
 // @namespace    luxysiv
-// @version      2023.01.12
+// @version      2023.01.13
 // @description  Blocks annoying elements in pages, sourced from many different filter lists
 // @author       luxysiv 
 // @match        *://*/*
@@ -14,7 +14,7 @@
 /// @stats blockers for 23170 domains, injected CSS rules for 3956 domains
 {
     let log = function (...data) {
-        console.log("[Cosmetic filters by luxysiv (v2023.01.12 full)]:", ...data);
+        console.log("[Cosmetic filters by luxysiv (v2023.01.13 full)]:", ...data);
     }
 
 

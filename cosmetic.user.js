5834d4eb1f088c0058bcbfe20f06f78edd95327d
Kingsmanvn-Official <Kingsmanvn-Official@users.noreply.github.com>
2024-10-13 00:53:06 +0000

    Cập nhật userscript: 2024.10.13-07.53.05

--- cosmetic.user.js
+++ cosmetic.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         Cosmetic Ad Block for Bromite
 // @namespace    kingsmanvn
-// @version      2024.10.12
+// @version      2024.10.13
 // @description  Blocks annoying elements in pages, sourced from many different filter lists
 // @author       kingsmanvn 
 // @match        *://*/*
@@ -14,7 +14,7 @@
 /// @stats blockers for 30473 domains, injected CSS rules for 4305 domains
 {
     let log = function (...data) {
-        console.log("[Cosmetic filters by luxysiv (v2024.10.12 full)]:", ...data);
+        console.log("[Cosmetic filters by luxysiv (v2024.10.13 full)]:", ...data);
     }
 
 
